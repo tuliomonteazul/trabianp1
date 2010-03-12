ff0fa26c92debd357bbe072d9f7c43e85fc2f324
tuliomonteazul <tuliomonteazul@acc9ef72-2d1e-11df-aa2f-955c74fa5d66>
2010-03-12 00:01:44 +0000

    depois da aula de 5a (11/03)

---  trabianp1/Trabalho IA - Poupador2008/src/algoritmo/Ladrao.java
+++  trabianp1/Trabalho IA - Poupador2008/src/algoritmo/Ladrao.java
@@ -1,88 +1,96 @@
 package algoritmo;
 
 public class Ladrao extends ProgramaLadrao {
+	// valores da ação
 	private static final int NAD = 0;
-	private static final int NOR = 1;
-	private static final int SUL = 2;
-	private static final int LES = 3;
-	private static final int OES = 4;
+	private static final int CIM = 1;
+	private static final int BAI = 2;
+	private static final int DIR = 3;
+	private static final int ESQ = 4;
 	
-	private static final int[] MTZ_POSICIONAR_DR = 
-		{
-		NAD, LES, LES, LES, LES,
-		SUL, LES, LES, LES, LES,
-		SUL, LES, LES, LES, NAD,
-		NAD, NAD, NAD, NAD, NAD,
-		NAD, NAD, NAD, NAD, NAD,
-		};
+	// valores da visão
+	private static final int SEM_VISAO = -2;
+	private static final int FORA_AMBIENTE = -1;
+	private static final int CELULA_VAZIA = 0;
+	private static final int PAREDE = 1;
+	private static final int BANCO = 3;
+	private static final int MOEDA = 4;
+	private static final int PASTILHA = 5;
+	private static final int POUPADOR = 100;
+	private static final int LADRAO = 200;
+	
+	
+	// valores ponderados
+	private static final int OLFATO_POUP = 10;
+	private static final int OLFATO_LADRAO = -10;
+	private static final int VISAO_PAREDE = -20;
+	private static final int VISAO_POUP = 20;
+	private static final int VISAO_LADRAO = -10;
+	private static final int VISAO_MOEDA = -10;
+	private static final int VISAO_BANCO = -10;
 	
 	private int ultimoMov = 0;
-	private boolean dRelacional;
 	
 	public int acao() {
+		
+		int[] mtzDecisao = {
+				0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0
+		};
+		
 		int i = 0;
 		int retorno = (int) (Math.random() * 5);
 		int maiorOlfatoPoupador = 0;
 
 		for (int olfato : sensor.getAmbienteOlfatoPoupador()){
 			// se o olfato for o maior (mais recente)
-			if (olfato > maiorOlfatoPoupador) {
+			if (olfato < maiorOlfatoPoupador && olfato != 0) {
 				maiorOlfatoPoupador = olfato;
 				// poupador passou pela esquerda
 				if (i == 3) {
-					retorno = OES;
-					System.out.println("olfato - movendo pra esquerda");
+					retorno = ESQ;
 				
 				// poupador passou pela esquerda e por cima
 				}else if (i == 0) {
-					retorno = NOR;
-					if (ultimoMov == NOR) {
-						retorno = OES;
-						System.out.println("olfato - movendo pra esquerda");
-					} else
-						System.out.println("olfato - movendo pra cima");
+					retorno = CIM;
+					if (ultimoMov == CIM) {
+						retorno = ESQ;
+					}
 					
 				// poupador passou por cima
 				}else if (i == 1) {
-					retorno = NOR;
-					System.out.println("olfato - movendo pra cima");
+					retorno = CIM;
 					
 				// poupador passou pela direita e por cima
 				}else if (i == 0) {
-					retorno = NOR;
-					if (ultimoMov == NOR) {
-						retorno = LES;
-						System.out.println("olfato - movendo pra direita");
-					} else
-						System.out.println("olfato - movendo pra cima");
+					retorno = CIM;
+					if (ultimoMov == CIM) {
+						retorno = DIR;
+					}
 				
 				// poupador a direita
 				} else if (i == 4) {
-					retorno = LES;
-					System.out.println("olfato - movendo pra direita");
+					retorno = DIR;
 						
 				// poupador passou pela esquerda e por baixo
 				}else if (i == 0) {
-					retorno = SUL;
-					if (ultimoMov == SUL) {
-						retorno = OES;
-						System.out.println("olfato - movendo pra esquerda");
-					} else
-						System.out.println("olfato - movendo pra baixo");
-					
+					retorno = BAI;
+					if (ultimoMov == BAI) {
+						retorno = ESQ;
+					}
 				// poupador a baixo
 				} else if (i == 6) {
-					retorno = SUL;
-					System.out.println("olfato - movendo pra baixo");
+					retorno = BAI;
 					
 				// poupador passou pela direita e por baixo
 				}else if (i == 0) {
-					retorno = SUL;
-					if (ultimoMov == SUL) {
-						retorno = LES;
-						System.out.println("olfato - movendo pra direita");
-					} else
-						System.out.println("olfato - movendo pra baixo");
+					retorno = BAI;
+					if (ultimoMov == BAI) {
+						retorno = DIR;
+					}
 				}
 			}
 			
@@ -96,72 +104,63 @@ public class Ladrao extends ProgramaLadrao {
 			if (visao >= 100 && visao <= 190) {
 				// poupador a esquerda
 				if (i == 10) {
-					retorno = OES;
-					System.out.println("movendo pra esquerda");
+					retorno = ESQ;
 				}else if (i < 10) {
 					// poupador a cima
 					if (i == 2) {
-						retorno = NOR;
-						System.out.println("movendo pra cima");
+						retorno = CIM;
 						
 					// poupador a esquerda e a cima
 					} else if (i % 5 <= 1) {
-						retorno = OES;
-						System.out.println("movendo pra esquerda");
+						retorno = ESQ;
 						
 					// poupador a direita e a cima
 					} else {
-						retorno = LES;
-						System.out.println("movendo pra direita");
+						retorno = DIR;
 					}
 					
 				// poupador a direita
 				} else if (i == 13) {
-					retorno = LES;
-					System.out.println("movendo pra direita");
+					retorno = DIR;
 				
 				// poupador a baixo
 				} else if (i == 21) {
-					retorno = SUL;
-					System.out.println("movendo pra baixo");
+					retorno = BAI;
 					
 				// poupador a esquerda e a baixo
 				} else if (i % 5 <= 1) {
-					retorno = OES;
-					System.out.println("movendo pra esquerda");
+					retorno = ESQ;
 			
 				// poupador a direita e a baixo
 				} else {
-					retorno = LES;
-					System.out.println("movendo pra direita");
+					retorno = DIR;
 				}
 			}
 			
 			
 			i++;
 		}
-		
-		retorno = verificarDRelacional(retorno);
 		
 		ultimoMov = retorno;
 		return retorno;
 	}
 
-	private int verificarDRelacional(int retornoAtual) {
+	private int verificarVisao(int retornoAtual, int[] mtzDecisao) {
 		
 		int retorno = retornoAtual;
 		int i = 0;
 		for (int visao : sensor.getVisaoIdentificacao()){
 			// se houver outro ladrao
-			if (visao >= 200 && visao <= 290) {
-				if (i == 0 || i == 4 || i == 19 || i == 23) {
-					dRelacional = true;
-					System.out.println("dRELACIONAL");
-				}
-				// se nao estiver em dRelacional
-//				if (!dRelacional) {
-					retorno = MTZ_POSICIONAR_DR[i];
-//				}
+			if (visao >= LADRAO) {
+				mtzDecisao[i] = VISAO_LADRAO; 
+			} else if (visao >= POUPADOR && visao < LADRAO) {
+				mtzDecisao[i] = VISAO_POUP;
+			} else if (visao == PAREDE) {
+				mtzDecisao[i] = VISAO_PAREDE;
+			} else if (visao == MOEDA || visao == PASTILHA) {
+				mtzDecisao[i] = VISAO_MOEDA;
+			} else if (visao == BANCO) {
+				mtzDecisao[i] = VISAO_MOEDA;
 			}
 			
 			
@@ -170,5 +169,5 @@ public class Ladrao extends ProgramaLadrao {
 		return retorno;
 	}
 	
-	
+
 }
